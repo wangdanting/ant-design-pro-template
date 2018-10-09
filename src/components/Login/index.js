a73f88ce06438cd37462e28a40c7912ef581a4c9
wangdanting <18875082742@163.com>
2018-10-09 14:06:06 +0800

    LoginContext

---
+++ src/components/Login/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import LoginContext from './LoginContext';
+
+
+
+class Login extends Component {
+  // 设置中间传递数据
+getContext = () => {
+  
+}
+
+render() {
+  
+}
+
+  <LoginContext.Provider value={this.getContext()}>
+  </LoginContext.Provider>
+}
+
+export default Form.create()(Login);
---
+++ src/pages/User/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'dva';
+import Link from 'umi/link';
+import styles from './Login.less';
+
+class LoginPage extends Component {
+  <div className={styles.main}>
+    
+  </div>
+}
+
+export default LoginPage;
+
