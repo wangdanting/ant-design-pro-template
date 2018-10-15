b24b9bcf9fbc5c69f890a3dafdbd6316231a5889
wangdanting <18875082742@163.com>
2018-10-15 17:26:16 +0800

    Register form

--- src/pages/User/Register.js
+++ src/pages/User/Register.js
@@ -1,38 +1,149 @@
-import React, { Component } from 'react';
-import styles from './Register.less';
-import { formatMessage, FormattedMessage } from 'umi/locale';
-import { Form, Input } from 'antd';
+import React, { Component } from "react";
+import styles from "./Register.less";
+import { formatMessage, FormattedMessage } from "umi/locale";
+import Link from "umi/link";
+import { Form, Input, Popover, Progress, Select, Button, Row, Col } from "antd";
 
 const FormItem = Form.Item;
+const InputGroup = Input.Group;
+const Option = Select.Option;
+
+const passwordStatusMap = {
+  ok: (
+    <div className={styles.success}>
+      <FormattedMessage id="validation.password.strength.strong" />
+    </div>
+  ),
+  pass: (
+    <div className={styles.warning}>
+      <FormattedMessage id="validation.password.strength.medium" />
+    </div>
+  ),
+  poor: (
+    <div className={styles.error}>
+      <FormattedMessage id="validation.password.strength.short" />
+    </div>
+  )
+};
+
+const passwordProgressMap = {
+  ok: "success",
+  pass: "normal",
+  poor: "exception"
+};
 
 class Register extends Component {
   state = {
-    help: ''
+    count: 0,
+    confirmDirty: false,
+    visible: false,
+    help: "",
+    prefix: "86"
   };
 
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
   checkPassword = (rule, value, callback) => {
+    const { visible, confirmDirty } = this.state;
     if (!value) {
       this.setState({
-        help: formatMessage({ id: 'validation.password.required' }),
+        help: formatMessage({ id: "validation.password.required" }),
         visible: !!value
       });
-      callback('error');
+      callback("error");
     } else {
       this.setState({
-        help: ''
+        help: ""
       });
       if (!visible) {
         this.setState({
-          visible: !!value,
+          visible: !!value
         });
       }
-    
+      if (value.length < 6) {
+        callback("error");
+      } else {
+        const { form } = this.props;
+        if (value && confirmDirty) {
+          form.validateFields(["confirm"], { force: true });
+        }
+        callback();
+      }
     }
-  }
+  };
+
+  getPasswordStatus = () => {
+    const { form } = this.props;
+    const value = form.getFieldValue("password");
+    if (value && value.length > 9) {
+      return "ok";
+    }
+    if (value && value.length > 5) {
+      return "pass";
+    }
+    return "poor";
+  };
+
+  renderPasswordProgress = () => {
+    const { form } = this.props;
+    const value = form.getFieldValue("password");
+    const passwordStatus = this.getPasswordStatus();
+    return value && value.length ? (
+      <div className={styles[`progress-${passwordStatus}`]}>
+        <Progress
+          status={passwordProgressMap[passwordStatus]}
+          className={styles.Progress}
+          strokeWidth={6}
+          percent={value.length * 10 > 100 ? 100 : value.length * 10}
+          showInfo={false}
+        />
+      </div>
+    ) : null;
+  };
+
+  checkConfirm = (rule, value, callback) => {
+    const { form } = this.props;
+    if (value && value !== form.getFieldValue("password")) {
+      callback(formatMessage({ id: "validation.password.twice" }));
+    } else {
+      callback();
+    }
+  };
+
+  changePrefix = value => {
+    this.setState({
+      prefix: value
+    });
+  };
+
+  onGetCaptcha = () => {
+    let count = 59;
+    this.setState({ count });
+    this.interval = setInterval(() => {
+      count -= 1;
+      this.setState({ count });
+      if (count === 0) {
+        clearInterval(this.interval);
+      }
+    }, 1000);
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { form, dispatch } = this.props;
+    form.validateFields({ force: true }, (err, values) => {
+      if (!err) {
+        const {}
+      }
+    });
+  };
 
   render() {
     const { form, submitting } = this.props;
     const { getFieldDecorator } = form;
+    const { help, visible, prefix, count } = this.state;
 
     return (
       <div className={styles.main}>
@@ -41,39 +152,172 @@ class Register extends Component {
         </h3>
         <Form onSubmit={this.handleSubmit}>
           <FormItem>
-            {getFieldDecorator('mail', {
+            {getFieldDecorator("mail", {
               rules: [
                 {
                   required: true,
-                  message: formatMessage({ id: 'validation.email.required' }),
+                  message: formatMessage({ id: "validation.email.required" })
                 },
                 {
-                  type: 'email',
-                  message: formatMessage({ id: 'validation.email.wrong-format' }),
+                  type: "email",
+                  message: formatMessage({
+                    id: "validation.email.wrong-format"
+                  })
                 }
-              ],
+              ]
             })(
-              <Input size="large" placeholder={formatMessage({ id: 'form.email.placeholder' })} />
+              <Input
+                size="large"
+                placeholder={formatMessage({ id: "form.email.placeholder" })}
+              />
             )}
           </FormItem>
           <FormItem help={help}>
-            {getFieldDecorator('password', {
+            <Popover
+              content={
+                <div style={{ padding: "4px 0" }}>
+                  {passwordStatusMap[this.getPasswordStatus()]}
+                  {this.renderPasswordProgress()}
+                  <div style={{ marginTop: 10 }}>
+                    <FormattedMessage id="validation.password.strength.msg" />
+                  </div>
+                </div>
+              }
+              overlayStyle={{ width: 240 }}
+              placement="right"
+              visible={visible}
+            >
+              {getFieldDecorator("password", {
+                rules: [
+                  {
+                    validator: this.checkPassword
+                  }
+                ]
+              })(
+                <Input
+                  size="large"
+                  type="password"
+                  placeholder={formatMessage({
+                    id: "form.password.placeholder"
+                  })}
+                />
+              )}
+            </Popover>
+          </FormItem>
+          <FormItem>
+            {getFieldDecorator("confirm", {
               rules: [
                 {
-                  validator: this.checkPassword,
+                  required: true,
+                  message: formatMessage({
+                    id: "validation.confirm-password.required"
+                  })
+                },
+                {
+                  validator: this.checkConfirm
                 }
-              ],
+              ]
             })(
               <Input
                 size="large"
                 type="password"
-                placeholder={formatMessage({ id: 'form.password.placeholder' })}
+                placeholder={formatMessage({
+                  id: "form.confirm-password.placeholder"
+                })}
               />
             )}
           </FormItem>
+          <FormItem>
+            <InputGroup compact>
+              <Select
+                size="large"
+                value={prefix}
+                onChange={this.changePrefix}
+                style={{ width: "20%" }}
+              >
+                <Option value="86">+86</Option>
+                <Option value="87">+87</Option>
+              </Select>
+              {getFieldDecorator("mobile", {
+                rules: [
+                  {
+                    required: true,
+                    message: formatMessage({
+                      id: "validation.phone-number.required"
+                    })
+                  },
+                  {
+                    pattern: /^\d{10}$/,
+                    message: formatMessage({
+                      id: "validation.phone-number.wrong-format"
+                    })
+                  }
+                ]
+              })(
+                <Input
+                  size="large"
+                  style={{ width: "80%" }}
+                  placeholder={formatMessage({
+                    id: "form.phone-number.placeholder"
+                  })}
+                />
+              )}
+            </InputGroup>
+          </FormItem>
+          <FormItem>
+            <Row gutter={8}>
+              <Col span={16}>
+                {getFieldDecorator("captcha", {
+                  rules: [
+                    {
+                      required: true,
+                      message: formatMessage({
+                        id: "validation.verification-code.required"
+                      })
+                    }
+                  ]
+                })(
+                  <Input
+                    size="large"
+                    placeholder={formatMessage({
+                      id: "form.verification-code.placeholder"
+                    })}
+                  />
+                )}
+              </Col>
+              <Col span={8}>
+                <Button
+                  size="large"
+                  disabled={count}
+                  className={styles.getCaptcha}
+                  onClick={this.onGetCaptcha}
+                >
+                  {count
+                    ? `${count} s`
+                    : formatMessage({
+                        id: "app.register.get-verification-code"
+                      })}
+                </Button>
+              </Col>
+            </Row>
+          </FormItem>
+          <FormItem>
+            <Button
+              size="large"
+              loading={submitting}
+              className={styles.submit}
+              type="primary"
+              htmlType="submit"
+            >
+              <FormattedMessage id="app.register.register" />
+            </Button>
+            <Link className={styles.login} to="User/Login">
+              <FormattedMessage id="app.register.sing-in" />
+            </Link>
+          </FormItem>
         </Form>
       </div>
-    )
+    );
   }
 }
 
