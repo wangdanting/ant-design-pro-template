06e13a9394d8b8523bbec5b3297d880ab2516dc9
wangdanting <18875082742@163.com>
2018-10-17 14:29:13 +0800

    BasicLayout

---
+++ src/layouts/BasicLayout.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+
+class BasicLayout extends React.PureComponent {
+  render() {
+    return (
+      <Fragment>
+        
+      </Fragment>
+    )
+  }
+}
+
+export default BasicLayout;
