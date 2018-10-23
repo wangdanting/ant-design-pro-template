cfb2acc8ee43763a047ce2c33ab5b5e1167dc55c
wangdanting <18875082742@163.com>
2018-10-23 17:54:06 +0800

    使用文档

--- src/components/GlobalHeader/RightContent.js
+++ src/components/GlobalHeader/RightContent.js
@@ -1,7 +1,9 @@
 import React, { PureComponent } from "react";
 import styles from "./index.less";
 import HeaderSearch from "../HeaderSearch";
-import classNames from 'classnames';
+import classNames from "classnames";
+import { formatMessage } from "umi/locale";
+import { Tooltip, Icon } from "antd";
 
 class GlobalHeaderRight extends PureComponent {
   render() {
@@ -12,7 +14,31 @@ class GlobalHeaderRight extends PureComponent {
     }
     return (
       <div className={className}>
-        <HeaderSearch className={classNames(styles.action, styles.search)} />
+        <HeaderSearch
+          className={classNames(styles.action, styles.search)}
+          placeholder={formatMessage({ id: "component.globalHeader.search" })}
+          dataSource={[
+            formatMessage({ id: "component.globalHeader.search.example1" }),
+            formatMessage({ id: "component.globalHeader.search.example2" }),
+            formatMessage({ id: "component.globalHeader.search.example3" })
+          ]}
+          onSearch={value => {
+            console.log("input", value); // eslint-disable-line
+          }}
+          onPressEnter={value => {
+            console.log("enter", value); // eslint-disable-line
+          }}
+        />
+        <Tooltip title={formatMessage({ id: 'component.globalHeader.help' })}>
+          <a
+            target="_blank"
+            href="https://pro.ant.design/docs/getting-started"
+            rel="noopener noreferrer"
+            className={styles.action}
+          >
+            <Icon type="question-circle-o" />
+          </a>
+        </Tooltip>
       </div>
     );
   }
