b5e84c13a5c490ee88138eb804d8ec7134927f6b
wangdanting <18875082742@163.com>
2018-11-06 23:01:17 +0800

    查询列表完成

--- src/pages/List/TableList.js
+++ src/pages/List/TableList.js
@@ -16,7 +16,9 @@ import {
   Divider,
   Dropdown,
   Menu,
-  Steps
+  Steps,
+  Radio,
+  message
 } from "antd";
 import styles from "./TableList.less";
 import { connect } from "dva";
@@ -26,6 +28,8 @@ const Step = Steps.Step;
 
 const FormItem = Form.Item;
 const { Option } = Select;
+const RadioGroup = Radio.Group;
+const { TextArea } = Input;
 const getValue = obj =>
   Object.keys(obj)
     .map(key => obj[key])
@@ -83,15 +87,182 @@ class UpdateForm extends PureComponent {
       },
       currentStep: 0
     };
+
+    this.formLayout = {
+      labelCol: { span: 7 },
+      wrapperCol: { span: 13 }
+    };
   }
+
+  backward = () => {
+    this.setState(prevState => ({
+      currentStep: prevState.currentStep - 1
+    }));
+  };
+
+  forward = () => {
+    this.setState(prevState => ({
+      currentStep: prevState.currentStep + 1
+    }));
+  };
+
+  handleNext = currentStep => {
+    const { form, handleUpdate } = this.props;
+    const { formVals: oldValue } = this.state;
+    form.validateFields((err, fieldsValue) => {
+      if (err) return;
+      const formVals = { ...oldValue, ...fieldsValue };
+      this.setState(
+        {
+          formVals
+        },
+        () => {
+          if (currentStep < 2) {
+            this.forward();
+          } else {
+            handleUpdate(formVals);
+          }
+        }
+      );
+    });
+  };
 
   renderContent = (currentStep, formVals) => {
     const { form } = this.props;
-    return <div>ddd</div>;
+    if (currentStep === 1) {
+      return [
+        <FormItem key="target" {...this.formLayout} label="监控对象">
+          {form.getFieldDecorator("target", {
+            initialValue: formVals.target
+          })(
+            <Select style={{ width: "100%" }}>
+              <Option value="0">表一</Option>
+              <Option value="1">表二</Option>
+            </Select>
+          )}
+        </FormItem>,
+        <FormItem key="template" {...this.formLayout} label="规则模板">
+          {form.getFieldDecorator("template", {
+            initialValue: formVals.template
+          })(
+            <Select style={{ width: "100%" }}>
+              <Option value="0">规则模板一</Option>
+              <Option value="1">规则模板二</Option>
+            </Select>
+          )}
+        </FormItem>,
+        <FormItem key="type" {...this.formLayout} label="规则类型">
+          {form.getFieldDecorator("type", {
+            initialValue: formVals.type
+          })(
+            <RadioGroup>
+              <Radio value="0">强</Radio>
+              <Radio value="1">弱</Radio>
+            </RadioGroup>
+          )}
+        </FormItem>
+      ];
+    }
+    if (currentStep === 2) {
+      return [
+        <FormItem key="item" {...this.formLayout} label="开始时间">
+          {form.getFieldDecorator("time", {
+            rules: [{ required: true, message: "请选择开始时间！" }]
+          })(
+            <DatePicker
+              style={{ width: "100%" }}
+              showTime
+              format="YYYY-MM-DD HH:mm:ss"
+              placeholder="选择开始时间"
+            />
+          )}
+        </FormItem>,
+        <FormItem key="frequency" {...this.formLayout} label="调度周期">
+          {form.getFieldDecorator("frequency", {
+            initialValue: formVals.frequency
+          })(
+            <Select style={{ width: "100%" }}>
+              <Option value="month">月</Option>
+              <Option value="week">周</Option>
+            </Select>
+          )}
+        </FormItem>
+      ];
+    }
+    return [
+      <FormItem key="name" {...this.formLayout} label="规则名称">
+        {form.getFieldDecorator("name", {
+          rules: [{ required: true, message: "请输入规则名称！" }],
+          initialValue: formVals.name
+        })(<Input placeholder="请输入" />)}
+      </FormItem>,
+      <FormItem key="desc" {...this.formLayout} label="规则描述">
+        {form.getFieldDecorator("desc", {
+          rules: [
+            {
+              required: true,
+              message: "请输入至少五个字符的规则描述！",
+              min: 5
+            }
+          ],
+          initialValue: formVals.desc
+        })(<TextArea rows={4} placeholder="请输入至少五个字符" />)}
+      </FormItem>
+    ];
+  };
+
+  renderFooter = currentStep => {
+    const { handleUpdateModalVisible } = this.props;
+    if (currentStep === 1) {
+      return [
+        <Button key="back" style={{ float: "left" }} onClick={this.backward}>
+          上一步
+        </Button>,
+        <Button key="cancel" onClick={() => handleUpdateModalVisible()}>
+          取消
+        </Button>,
+        <Button
+          key="forward"
+          type="primary"
+          onClick={() => this.handleNext(currentStep)}
+        >
+          下一步
+        </Button>
+      ];
+    }
+    if (currentStep === 2) {
+      return [
+        <Button key="back" style={{ float: "left" }} onClick={this.backward}>
+          上一步
+        </Button>,
+        <Button key="cancel" onClick={() => handleUpdateModalVisible()}>
+          取消
+        </Button>,
+        <Button
+          key="submit"
+          type="primary"
+          onClick={() => this.handleNext(currentStep)}
+        >
+          完成
+        </Button>
+      ];
+    }
+    return [
+      <Button key="cancel" onClick={() => handleUpdateModalVisible()}>
+        取消
+      </Button>,
+      <Button
+        key="forward"
+        type="primary"
+        onClick={() => this.handleNext(currentStep)}
+      >
+        下一步
+      </Button>
+    ];
   };
 
   render() {
-    const { updateModalVisible } = this.props;
+    const { updateModalVisible, handleUpdateModalVisible } = this.props;
     const { currentStep, formVals } = this.state;
 
     return (
@@ -100,13 +271,15 @@ class UpdateForm extends PureComponent {
         bodyStyle={{ padding: "32px 40px 48px" }}
         title="规则配置"
         destroyOnClose
-        visible={updateModalVisible}
+        visible={true}
+        footer={this.renderFooter(currentStep)}
+        onCancel={() => handleUpdateModalVisible()}
       >
-        {/* <Steps style={{ marginBottom: 28 }} size="small" current={currentStep}>
+        <Steps style={{ marginBottom: 28 }} size="small" current={currentStep}>
           <Step title="基本信息" />
           <Step title="配置规则属性" />
           <Step title="设定调度周期" />
-        </Steps> */}
+        </Steps>
         {this.renderContent(currentStep, formVals)}
       </Modal>
     );
@@ -453,6 +626,22 @@ class TableList extends PureComponent {
       rule: { data },
       loading
     } = this.props;
+    const abc = {
+      avatar:
+        "https://gw.alipayobjects.com/zos/rmsportal/eeHMaZBwmTvLdIwMfBpg.png",
+      callNo: 138,
+      createdAt: "2017-07-22T00:00:00.000Z",
+      desc: "这是一段描述",
+      disabled: true,
+      href: "https://ant.design",
+      key: 42,
+      name: "TradeCode 42",
+      owner: "曲丽丽",
+      progress: 39,
+      status: 0,
+      title: "一个任务名称 42",
+      updatedAt: "2017-07-22T00:00:00.000Z"
+    };
     const menu = (
       <Menu onClick={this.handleMenuClick} selectedKeys={[]}>
         <Menu.Item key="remove">删除</Menu.Item>
