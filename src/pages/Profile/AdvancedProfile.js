67259ee5bc8abbccbacff77cd75cd181d7c2bc48
wangdanting <18875082742@163.com>
2018-11-11 00:13:19 +0800

    异常页

--- src/pages/Profile/AdvancedProfile.js
+++ src/pages/Profile/AdvancedProfile.js
@@ -2,7 +2,10 @@ import React, { PureComponent } from "react";
 import { connect } from "dva";
 
 import styles from "./AdvancedProfile.less";
-
+@connect(({ profile, loading }) => ({
+  profile,
+  loading: loading.effects["profile/fetchAdvanced"]
+}))
 class AdvancedProfile extends PureComponent {
   render() {
     return <div>dd</div>;
