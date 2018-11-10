2a6de50098ce06b5e8b7e77d475c35dbfb039d98
wangdanting <18875082742@163.com>
2018-11-10 11:44:24 +0800

    准备写AvatarList

---
+++ src/components/AvatarList/index.js
@@ -0,0 +1,6 @@
+import React, { PureComponent } from "react";
+import styles from "./index.less";
+
+const AvatarList = () => <div>dd</div>;
+
+export default AvatarList;
--- src/pages/List/Projects.js
+++ src/pages/List/Projects.js
@@ -1,9 +1,143 @@
 import React, { PureComponent } from "react";
 import styles from "./Projects.less";
+import StandardFormRow from "@/components/StandardFormRow";
+import TagSelect from "@/components/TagSelect";
+import AvatarList from "@/components/AvatarList";
+import Ellipsis from "@/components/Ellipsis";
+import { Row, Col, Form, Card, Select, List } from "antd";
+import moment from "moment";
+import { connect } from "dva";
 
+const { Option } = Select;
+const FormItem = Form.Item;
+
+const formItemLayout = {
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 }
+  }
+};
+
+@connect(({ list, loading }) => ({
+  list,
+  loading: loading.models.list
+}))
+@Form.create()
 class Projects extends PureComponent {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch({
+      type: "list/fetch",
+      payload: {
+        count: 8
+      }
+    });
+  }
+
   render() {
-    return <div>Projects</div>;
+    const {
+      list: { list = [] },
+      loading,
+      form: { getFieldDecorator }
+    } = this.props;
+
+    const cardList = list ? (
+      <List
+        rowKey="id"
+        loading={loading}
+        grid={{ gutter: 24, xl: 4, lg: 3, md: 3, sm: 2, xs: 1 }}
+        dataSource={list}
+        renderItem={item => (
+          <List.Item>
+            <Card
+              className={styles.card}
+              hoverable
+              cover={<img alt={item.title} src={item.cover} />}
+            >
+              <Card.Meta
+                title={<a>{item.title}</a>}
+                description={
+                  <Ellipsis lines={2}>{item.subDescription}</Ellipsis>
+                }
+              />
+              <div className={styles.cardItemContent}>
+                <span>{moment(item.updatedAt).fromNow()}</span>
+                <div className={styles.avatarList}>
+                  <AvatarList size="mini">
+                    {item.members.map((member, i) => (
+                      <div key={`${item.id}-avatar-${i}`}>dd</div>
+                    ))}
+                  </AvatarList>
+                </div>
+              </div>
+            </Card>
+          </List.Item>
+        )}
+      />
+    ) : null;
+
+    return (
+      <div className={styles.coverCardList}>
+        <Card bordered={false}>
+          <Form layout="inline">
+            <StandardFormRow
+              title="所属类目"
+              block
+              style={{ paddingBottom: 11 }}
+            >
+              <FormItem>
+                {getFieldDecorator("category")(
+                  <TagSelect expandable>
+                    <TagSelect.Option value="cat1">类目一</TagSelect.Option>
+                    <TagSelect.Option value="cat2">类目二</TagSelect.Option>
+                    <TagSelect.Option value="cat3">类目三</TagSelect.Option>
+                    <TagSelect.Option value="cat4">类目四</TagSelect.Option>
+                    <TagSelect.Option value="cat5">类目五</TagSelect.Option>
+                    <TagSelect.Option value="cat6">类目六</TagSelect.Option>
+                    <TagSelect.Option value="cat7">类目七</TagSelect.Option>
+                    <TagSelect.Option value="cat8">类目八</TagSelect.Option>
+                    <TagSelect.Option value="cat9">类目九</TagSelect.Option>
+                    <TagSelect.Option value="cat10">类目十</TagSelect.Option>
+                    <TagSelect.Option value="cat11">类目十一</TagSelect.Option>
+                    <TagSelect.Option value="cat12">类目十二</TagSelect.Option>
+                  </TagSelect>
+                )}
+              </FormItem>
+            </StandardFormRow>
+            <StandardFormRow title="其它选项" grid last>
+              <Row gutter={16}>
+                <Col lg={8} md={10} sm={10} xs={24}>
+                  <FormItem {...formItemLayout} label="作者">
+                    {getFieldDecorator("author", {})(
+                      <Select
+                        placeholder="不限"
+                        style={{ maxWidth: 200, width: "100%" }}
+                      >
+                        <Option value="lisa">王昭君</Option>
+                      </Select>
+                    )}
+                  </FormItem>
+                </Col>
+                <Col lg={8} md={10} sm={10} xs={24}>
+                  <FormItem {...formItemLayout} label="好评度">
+                    {getFieldDecorator("rate", {})(
+                      <Select
+                        placeholder="不限"
+                        style={{ maxWidth: 200, width: "100%" }}
+                      >
+                        <Option value="good">优秀</Option>
+                        <Option value="normal">普通</Option>
+                      </Select>
+                    )}
+                  </FormItem>
+                </Col>
+              </Row>
+            </StandardFormRow>
+          </Form>
+        </Card>
+        <div className={styles.cardList}>{cardList}</div>
+      </div>
+    );
   }
 }
 
