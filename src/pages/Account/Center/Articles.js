293cfb34b7a417990ca891455b77cd1ac272f252
wangdanting <18875082742@163.com>
2018-11-12 14:59:59 +0800

    个人中心 文章

--- src/pages/Account/Center/Articles.js
+++ src/pages/Account/Center/Articles.js
@@ -1,9 +1,60 @@
 import React, { PureComponent } from "react";
-import styles from "./Center.less";
+import { connect } from "dva";
+import { List, Icon, Tag } from "antd";
+import ArticleListContent from "@/components/ArticleListContent";
+import styles from "./Article.less";
 
+@connect(({ list }) => ({
+  list
+}))
 class Articles extends PureComponent {
   render() {
-    return <div>Articles</div>;
+    const {
+      list: { list }
+    } = this.props;
+
+    const IconText = ({ type, text }) => (
+      <span>
+        <Icon type={type} style={{ marginRight: 8 }} />
+        {text}
+      </span>
+    );
+
+    return (
+      <List
+        size="large"
+        className={styles.articleList}
+        rowKey="id"
+        itemLayout="vertical"
+        dataSource={list}
+        renderItem={item => (
+          <List.Item
+            key={item.id}
+            actions={[
+              <IconText type="star-o" text={item.star} />,
+              <IconText type="like-o" text={item.like} />,
+              <IconText type="message" text={item.message} />
+            ]}
+          >
+            <List.Item.Meta
+              title={
+                <a className={styles.listItemMetaTitle} href={item.href}>
+                  {item.title}
+                </a>
+              }
+              description={
+                <span>
+                  <Tag>Ant Design</Tag>
+                  <Tag>设计语言</Tag>
+                  <Tag>蚂蚁金服</Tag>
+                </span>
+              }
+            />
+            <ArticleListContent data={item} />
+          </List.Item>
+        )}
+      />
+    );
   }
 }
 
