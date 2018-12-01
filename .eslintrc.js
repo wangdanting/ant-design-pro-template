1f81b7cfd965bc511de0a5d80794ba7091fd8e8c
wangdanting <18875082742@163.com>
2018-12-01 21:49:31 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,6 @@ module.exports = {
     browser: true,
     node: true,
     es6: true,
-    mocha: true,
-    jest: true,
-    jasmine: true,
-  },
-  globals: {
-    APP_TYPE: true,
-    page: true,
   },
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js'] }],
@@ -31,7 +24,7 @@ module.exports = {
     'jsx-a11y/click-events-have-key-events': 0,
     'jsx-a11y/no-static-element-interactions': 0,
     'jsx-a11y/anchor-is-valid': 0,
-    'linebreak-style': 0,
+    // 'linebreak-style': 0,
     'react/no-array-index-key': 0,
     'react/no-multi-comp': 0,
   },
